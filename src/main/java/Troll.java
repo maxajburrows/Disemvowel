babdf5102174c03c92924563b6af3cb92c50ee6e
Max Burrows <maxajburrows@gmail.com>
2023-05-30 19:48:02 +0200

    Optimise solution

--- src/main/java/Troll.java
+++ src/main/java/Troll.java
@@ -1,13 +1,5 @@
 public class Troll {
     public static String disemvowel(String str) {
-        String stringNoVowels = "";
-        String strToUpperCase = str.toUpperCase();
-        String vowels = "AEIOU";
-        for (int i = 0; i < strToUpperCase.length(); i++) {
-            if (!vowels.contains(String.valueOf(strToUpperCase.charAt(i)))) {
-                stringNoVowels += String.valueOf(str.charAt(i));
-            }
-        }
-        return stringNoVowels;
+        return str.replaceAll("(?i)[aeiou]", "");
     }
 }
